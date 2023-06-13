f7ccec1df442678b5a8e7cbbc492c87c8052b5af
Noiznor <124698043+Noiznor@users.noreply.github.com>
2023-06-13 09:08:55 +0800

    knight.java

--- knight.java
+++ knight.java
@@ -1,9 +1,11 @@
 
 import java.util.stream.*;
 
-public class knight {
+public class knight
+{
 
-    public static int knight(String...pos) {
+    public static int knight(String...pos) 
+    {
         int[][] ab=Stream.of(pos).map(s->new int[]{"abcdefgh".indexOf(s.charAt(0)),s.charAt(1)-48}).toArray(int[][]::new);
         int[] dxy=IntStream.range(0,2).map(i->Math.abs(ab[0][i]-ab[1][i])).sorted().toArray();
         if(dxy[0]==0&&dxy[1]==1) return 3;
